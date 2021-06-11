99d1287b2c6c46d2ec8892d45399835add721d0a
Ciara Swann <cccswann@gmail.com>
2021-06-10 17:00:28 -0700

    Add js for the back to top button.

---
+++ app/static/js/main.js
@@ -0,0 +1,20 @@
+// Get the button:
+mybutton = document.getElementById("topBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    var mybutton = document.getElementById("topBtn")
+  if (document.body.scrollTop > 10 || document.documentElement.scrollTop > 10) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
